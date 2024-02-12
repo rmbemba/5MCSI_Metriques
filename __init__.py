ca97cb249b113f1e9e40b2e50c595aa74b177fc8
rmbemba <159789910+rmbemba@users.noreply.github.com>
2024-02-12 16:18:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,11 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)    
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
                                                                                                                                        
 @app.route('/')
 def hello_world():
@@ -13,7 +17,3 @@ def hello_world():
   
 if __name__ == "__main__":
   app.run(debug=True)
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
